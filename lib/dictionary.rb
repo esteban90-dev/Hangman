5f051722168003627b0eed6f2d9c4424086394d2
Stephen Allard <stephencallard@gmail.com>
2020-04-07 07:59:16 -0500

    Added dictionary class

---
+++ lib/dictionary.rb
@@ -0,0 +1,26 @@
+class Dictionary
+  attr_reader :contents
+
+  def initialize(url="https://www.scrapmaker.com/data/wordlists/twelve-dicts/5desk.txt")
+    @contents = read_dictionary_file(url)
+  end
+
+  public
+
+  def random_word
+    select_words_by_length[rand(0..select_words_by_length.length)]
+  end
+
+  def select_words_by_length(range=(4..9))    #range is the default range of word lengths
+    contents.select{ |word| (word.length >= range.first) && (word.length <= range.last) }
+  end
+
+  private
+
+  def read_dictionary_file(url)
+    URI.open(url).read.split
+  end
+
+end
+  
+  
