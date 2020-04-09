0cc96ab5fc50631bc037da9662c8984cca707226
Stephen Allard <stephencallard@gmail.com>
2020-04-09 04:00:08 -0500

    added solicit_guess method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :dictionary, :secret
+  attr_reader :player, :dictionary, :secret
 
   def initialize(args)
     @player = args["player"]
@@ -19,7 +19,24 @@ class Game
      Try to figure out the unknown word 
      by guessing letters. If too many letters 
      which do not appear in the word are guessed, 
-     you will be hanged!""\n"
+     you will be hanged!"
+  end
+
+  def prompt_player
+    loop do
+      puts "Please enter a new letter."
+      break if good_input?(gets.chomp)
+    end
+  end
+
+  def good_input?(input)
+    return false if input.length != 1
+    return false if already_guessed?(input)
+    true
+  end
+
+  def already_guessed?(input)
+    player.guess.include?(input)
   end
 
 end
