9e5aa73aac00d19c205977084f5e8378b63f841e
Stephen Allard <stephencallard@gmail.com>
2020-04-12 05:01:45 -0500

    added user_input.rb module

---
+++ lib/user_input.rb
@@ -0,0 +1,15 @@
+module UserInput
+  TAB = "\n\s\s\s\s"
+  
+  def UserInput.get_user_input(prompt_message, regexp) #uses regular expression to verify user input
+    loop do
+      puts TAB + prompt_message
+      input = gets.chomp.downcase
+      if input.match?(regexp)
+        return input
+      else
+        puts TAB + "Invalid input."
+      end
+    end
+  end
+end
